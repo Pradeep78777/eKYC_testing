3e9a44c96924a905f93329088a5683c6c612d34a
GowriSankar-7 <gowrisankar@stoneagesolutions.com>
2020-10-10 19:25:56 +0530

    git check

--- src/com/codespine/util/eKYCConstant.java
+++ src/com/codespine/util/eKYCConstant.java
@@ -84,5 +84,5 @@ public class eKYCConstant {
 	public static final String TICK = "TICK";
 	public static final String COMMA_SEPERATOR = ",";
 	public static final String IMAGE_FILEPATH = "C:\\Users\\user\\Downloads\\";
-	
+	public static final String GIT_CHECK = "";	
 }
