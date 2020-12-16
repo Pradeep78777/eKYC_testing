edc17ae334f37579356e6533cd53308d93671b54
pradeep78777 <pradeep.r@stoneagesolutions.com>
2020-12-16 21:34:25 +0530

    image changes

--- src/com/codespine/util/Test.java
+++ src/com/codespine/util/Test.java
@@ -24,77 +24,104 @@ import org.imgscalr.Scalr;
 public class Test {
 	static String img1 = "http://rest.irongates.in/pic/0-1.jpg";
 	static String img2 = "http://rest.irongates.in/pic/1.png";
+
 	public static void main(String[] args) throws InvalidPasswordException, IOException {
 		String finalPDFName = "img.pdf";
-		File file = new File("C:\\Users\\prade\\Downloads\\ekyc_pdf\\2"+ eKYCConstant.PDF_FILE_EXTENSION);
+		File file = new File("C:\\Users\\prade\\Downloads\\ekyc_pdf\\2" + eKYCConstant.PDF_FILE_EXTENSION);
 		PDDocument document = PDDocument.load(file);
 		try {
-			pdfTextInserter(document,".",473,595);
-			pdfTextInserter(document,".",571,595);
-			pdfTextInserter(document,".",473,695);
-			pdfTextInserter(document,".",571,695);
+//			pdfTextInserter(document, ".", 474, 594);
+//			pdfTextInserter(document, ".", 574, 594);
+//			pdfTextInserter(document, ".", 474, 698);
+//			pdfTextInserter(document, ".", 574, 698);
+			pdfimageInserter(0, document, 474.5f, 594, img2, "1", "C:\\Users\\prade\\Downloads\\ekyc_pdf\\");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		pdfimageInserter(0,document,475,595,img2,"1","C:\\Users\\prade\\Downloads\\ekyc_pdf\\");
-		
-		document.save(new File("C:\\Users\\prade\\Downloads\\ekyc_pdf" + eKYCConstant.WINDOWS_FORMAT_SLASH + finalPDFName));
+
+		document.save(
+				new File("C:\\Users\\prade\\Downloads\\ekyc_pdf" + eKYCConstant.WINDOWS_FORMAT_SLASH + finalPDFName));
 		System.out.println("pdf Generated");
 		document.close();
 	}
-	private static void pdfimageInserter(int pageNumber,PDDocument document,int xValue, int yValue, String image,String application_id, String finalSestinationFilePath) throws IOException {
+
+	/**
+	 * 
+	 * @param pageNumber
+	 * @param document
+	 * @param xValue
+	 * @param yValue
+	 * @param image
+	 * @param application_id
+	 * @param finalSestinationFilePath
+	 * @throws IOException
+	 */
+	private static void pdfimageInserter(int pageNumber, PDDocument document, float xValue, int yValue, String image,
+			String application_id, String finalSestinationFilePath) throws IOException {
 		InputStream in = new URL(image).openStream();
 		BufferedImage bimg = ImageIO.read(in);
-		int height = bimg.getHeight(); //310
-		int width = bimg.getWidth(); //500
+		int height = bimg.getHeight(); // 310
+		int width = bimg.getWidth(); // 500
 		int scaledHeight = 100;
 		int scaledWidth = 100;
-		if(bimg.getWidth() > bimg.getHeight()) {
-			int ratio = bimg.getWidth()/100;
-			scaledHeight = bimg.getHeight()/ratio;
-			scaledWidth = 	100;	
-		}else {
-			int ratio = bimg.getHeight()/100;
-			scaledHeight =  100;
-			scaledWidth = bimg.getWidth()/ratio;
+		if (bimg.getWidth() > bimg.getHeight()) {
+			int ratio = bimg.getWidth() / 100;
+			scaledHeight = bimg.getHeight() / ratio;
+			scaledWidth = 100;
+		} else {
+			int ratio = bimg.getHeight() / 100;
+			scaledHeight = 100;
+			scaledWidth = bimg.getWidth() / ratio;
 		}
-		int changedXValue = 0;
-		int changedYValue = 0;
-		if(bimg.getWidth() <= 100) {
-			scaledWidth = bimg.getWidth(); 
+//		int changedXValue = 0;
+//		int changedYValue = 0;
+		if (bimg.getWidth() <= 100) {
+			scaledWidth = bimg.getWidth();
 		}
-		if(bimg.getHeight() <= 100) {
-			scaledHeight = bimg.getHeight(); 
+		if (bimg.getHeight() <= 100) {
+			scaledHeight = bimg.getHeight();
 		}
-		if(scaledWidth <= 100) {
-			if(scaledWidth > scaledHeight) {
-				changedYValue = (scaledWidth - scaledHeight)/2;
-			}else {
-				changedXValue = (scaledHeight - scaledWidth)/2;
-			}
-		}else {
-			if(scaledHeight > scaledWidth) {
-				changedXValue = (scaledHeight - scaledWidth)/2;
-			}else {
-				changedYValue = (scaledWidth - scaledHeight)/2;
-			}
+		
+		int xAxis = 0;
+		int yAxis = 0;
+		if(height > width) {
+			//then you move X axis
+//			xAxis = ((width - scaledWidth) / 2);
+			xAxis = ((scaledHeight - scaledWidth) / 2);
+		} else {
+			//Then you move Y axis
+//			yAxis = ((height - scaledHeight) / 2);
+			yAxis = ((scaledWidth - scaledHeight) / 2);
 		}
+//		if (scaledWidth <= 100) {
+//			if (scaledWidth > scaledHeight) {
+//				changedYValue = (scaledWidth - scaledHeight) / 2;
+////			}else {
+////				changedXValue = (scaledHeight - scaledWidth)/2;
+//			}
+//		} else {
+//			if (scaledHeight > scaledWidth) {
+//				changedXValue = (scaledHeight - scaledWidth) / 2;
+////			}else {
+////				changedYValue = (scaledWidth - scaledHeight)/2;
+//			}
+//		}
 		BufferedImage scaledImg = Scalr.resize(bimg, scaledWidth, scaledHeight);
 		ByteArrayOutputStream os = new ByteArrayOutputStream();
 		String imagePath = "";
 		String resizeImageName = image;
-		if(StringUtil.isStrContainsWithEqIgnoreCase(image, "png")) {
+		if (StringUtil.isStrContainsWithEqIgnoreCase(image, "png")) {
 			ImageIO.write(scaledImg, "png", os);
-			resizeImageName = StringUtil.isImageReSizeExist(finalSestinationFilePath, application_id,".png");
-			imagePath = finalSestinationFilePath+ eKYCConstant.WINDOWS_FORMAT_SLASH +resizeImageName;
-		}else {
+			resizeImageName = StringUtil.isImageReSizeExist(finalSestinationFilePath, application_id, ".png");
+			imagePath = finalSestinationFilePath + eKYCConstant.WINDOWS_FORMAT_SLASH + resizeImageName;
+		} else {
 			ImageIO.write(scaledImg, "jpeg", os);
-			resizeImageName = StringUtil.isImageReSizeExist(finalSestinationFilePath, application_id,".jpg");
-			imagePath = finalSestinationFilePath+ eKYCConstant.WINDOWS_FORMAT_SLASH + resizeImageName;
+			resizeImageName = StringUtil.isImageReSizeExist(finalSestinationFilePath, application_id, ".jpg");
+			imagePath = finalSestinationFilePath + eKYCConstant.WINDOWS_FORMAT_SLASH + resizeImageName;
 		}
 		InputStream is = new ByteArrayInputStream(os.toByteArray());
 		int read = 0;
-			OutputStream out = new FileOutputStream(imagePath);
+		OutputStream out = new FileOutputStream(imagePath);
 		byte[] bytes = new byte[1024];
 		while ((read = is.read(bytes)) != -1) {
 			out.write(bytes, 0, read);
@@ -103,27 +130,30 @@ public class Test {
 		out.close();
 		is.close();
 		os.close();
-		PDImageXObject  pdImage = null;
-		if(StringUtil.isStrContainsWithEqIgnoreCase(image, "png")) {
+		PDImageXObject pdImage = null;
+		if (StringUtil.isStrContainsWithEqIgnoreCase(image, "png")) {
 			File f = new File(imagePath);
 			BufferedImage bimg1 = ImageIO.read(f);
 			pdImage = LosslessFactory.createFromImage(document, bimg1);
-		}else {
+		} else {
 			pdImage = PDImageXObject.createFromFile(imagePath, document);
 		}
 		PDPage page = document.getPage(pageNumber);
-		PDPageContentStream contentStream = new PDPageContentStream(document, page,AppendMode.APPEND, true);
-		contentStream.drawImage(pdImage, xValue+changedXValue, yValue+changedYValue);
+		PDPageContentStream contentStream = new PDPageContentStream(document, page, AppendMode.APPEND, true);
+		contentStream.drawImage(pdImage, xValue + xAxis, yValue + yAxis);
 		contentStream.close();
-		in.close();	
+		in.close();
 
 	}
-	public static void pdfTextInserter(PDDocument document,String insertValue, int xValue, int yValue) throws Exception {
+
+	@SuppressWarnings("deprecation")
+	public static void pdfTextInserter(PDDocument document, String insertValue, int xValue, int yValue)
+			throws Exception {
 		PDPage page = document.getPage(0);
-	   	PDPageContentStream contentStream = new PDPageContentStream(document, page,AppendMode.APPEND, true);
+		PDPageContentStream contentStream = new PDPageContentStream(document, page, AppendMode.APPEND, true);
 		contentStream.beginText();
 		contentStream.setNonStrokingColor(0, 0, 0);
-		File f = new File("E:\\ekyc\\"+"MonospaceTypewriter.ttf");
+		File f = new File("E:\\ekyc\\" + "MonospaceTypewriter.ttf");
 		PDFont font1 = PDTrueTypeFont.loadTTF(document, f);
 //		if (resizeRequired > 0) {
 //			contentStream.setCharacterSpacing(0.4f);
@@ -137,7 +167,7 @@ public class Test {
 //			contentStream.setFont(font1, 8);
 //			contentStream.setCharacterSpacing(0.4f);
 //		}
-		contentStream.setFont(font1,8);
+		contentStream.setFont(font1, 8);
 		contentStream.setCharacterSpacing(0.4f);
 		contentStream.newLineAtOffset(xValue, yValue);
 		String text = insertValue;
