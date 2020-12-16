17ef7ba253b59878b364981637908f14e8941c62
pradeep78777 <pradeep.r@stoneagesolutions.com>
2020-12-16 18:51:31 +0530

    pdf image changes

--- src/com/codespine/util/Test.java
+++ src/com/codespine/util/Test.java
@@ -1,48 +1,148 @@
 package com.codespine.util;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
+import javax.imageio.ImageIO;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.PDPageContentStream.AppendMode;
+import org.apache.pdfbox.pdmodel.encryption.InvalidPasswordException;
+import org.apache.pdfbox.pdmodel.font.PDFont;
+import org.apache.pdfbox.pdmodel.font.PDTrueTypeFont;
+import org.apache.pdfbox.pdmodel.graphics.image.LosslessFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+import org.imgscalr.Scalr;
 
 public class Test {
-	public static final String DDMMYYYY_SLASH 				= "ABC:def";
-	
-	
-	public static void main(String[] args) {
-		SimpleDateFormat  formatter1 = null;
-		Date date = null;
+	static String img1 = "http://rest.irongates.in/pic/0-1.jpg";
+	static String img2 = "http://rest.irongates.in/pic/1.png";
+	public static void main(String[] args) throws InvalidPasswordException, IOException {
+		String finalPDFName = "img.pdf";
+		File file = new File("C:\\Users\\prade\\Downloads\\ekyc_pdf\\2"+ eKYCConstant.PDF_FILE_EXTENSION);
+		PDDocument document = PDDocument.load(file);
 		try {
-////			List<ApplicationLogDTO> result = AdminDAO.getInstance().rejectedDocuments(1);
-////			Utility.applicationRejectedMessage("gowrisankar@stoneagesolutions.com", "Gowrisankar", result);
-////			Utility.stringToJson("1^cntpk5744h^E^KRITHIHASRI^CHINNASWAMY^VENUGOPAL^Kumari^01/06/2012^KRITHIHASRI C V^Y^");
-////			System.out.println(DateUtil.formatDate(DateUtil.getTodayDate(), DDMMYYYY_SLASH));
-////			System.out.println(DateUtil.getLastFinancialYearTo(DateUtil.getTodayDate()));
-//			System.out.println(StringUtils.substringAfter(DDMMYYYY_SLASH, ":"));
-			String dateString = "1997-06-24";
-			formatter1 = new SimpleDateFormat(DateUtil.YYYYMMDD);
-			date = (Date) formatter1.parse((StringUtil.replace(dateString,"-","")));
-			System.out.println("date"+date);
-			String changedDate = formatDate(date,DateUtil.DDMMYYYY_SLASH);
-			System.out.println(changedDate);
-			
+			pdfTextInserter(document,".",473,595);
+			pdfTextInserter(document,".",571,595);
+			pdfTextInserter(document,".",473,695);
+			pdfTextInserter(document,".",571,695);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		pdfimageInserter(0,document,475,595,img2,"1","C:\\Users\\prade\\Downloads\\ekyc_pdf\\");
+		
+		document.save(new File("C:\\Users\\prade\\Downloads\\ekyc_pdf" + eKYCConstant.WINDOWS_FORMAT_SLASH + finalPDFName));
+		System.out.println("pdf Generated");
+		document.close();
 	}
-	public static String formatDate(Date date, String format){
-		DateFormat dateFormat = new SimpleDateFormat(format);
-		return formatDateByformat(date, dateFormat);
-	}
-	public static String formatDateByformat(Date date, DateFormat dateFormat){
-		String dateStr = null;
-		try {
-			if (date != null) {
-				dateStr = dateFormat.format(date);
-			}
-		}catch (Exception e) {
-			return null;
+	private static void pdfimageInserter(int pageNumber,PDDocument document,int xValue, int yValue, String image,String application_id, String finalSestinationFilePath) throws IOException {
+		InputStream in = new URL(image).openStream();
+		BufferedImage bimg = ImageIO.read(in);
+		int height = bimg.getHeight(); //310
+		int width = bimg.getWidth(); //500
+		int scaledHeight = 100;
+		int scaledWidth = 100;
+		if(bimg.getWidth() > bimg.getHeight()) {
+			int ratio = bimg.getWidth()/100;
+			scaledHeight = bimg.getHeight()/ratio;
+			scaledWidth = 	100;	
+		}else {
+			int ratio = bimg.getHeight()/100;
+			scaledHeight =  100;
+			scaledWidth = bimg.getWidth()/ratio;
 		}
-		return dateStr;
-	}
+		int changedXValue = 0;
+		int changedYValue = 0;
+		if(bimg.getWidth() <= 100) {
+			scaledWidth = bimg.getWidth(); 
+		}
+		if(bimg.getHeight() <= 100) {
+			scaledHeight = bimg.getHeight(); 
+		}
+		if(scaledWidth <= 100) {
+			if(scaledWidth > scaledHeight) {
+				changedYValue = (scaledWidth - scaledHeight)/2;
+			}else {
+				changedXValue = (scaledHeight - scaledWidth)/2;
+			}
+		}else {
+			if(scaledHeight > scaledWidth) {
+				changedXValue = (scaledHeight - scaledWidth)/2;
+			}else {
+				changedYValue = (scaledWidth - scaledHeight)/2;
+			}
+		}
+		BufferedImage scaledImg = Scalr.resize(bimg, scaledWidth, scaledHeight);
+		ByteArrayOutputStream os = new ByteArrayOutputStream();
+		String imagePath = "";
+		String resizeImageName = image;
+		if(StringUtil.isStrContainsWithEqIgnoreCase(image, "png")) {
+			ImageIO.write(scaledImg, "png", os);
+			resizeImageName = StringUtil.isImageReSizeExist(finalSestinationFilePath, application_id,".png");
+			imagePath = finalSestinationFilePath+ eKYCConstant.WINDOWS_FORMAT_SLASH +resizeImageName;
+		}else {
+			ImageIO.write(scaledImg, "jpeg", os);
+			resizeImageName = StringUtil.isImageReSizeExist(finalSestinationFilePath, application_id,".jpg");
+			imagePath = finalSestinationFilePath+ eKYCConstant.WINDOWS_FORMAT_SLASH + resizeImageName;
+		}
+		InputStream is = new ByteArrayInputStream(os.toByteArray());
+		int read = 0;
+			OutputStream out = new FileOutputStream(imagePath);
+		byte[] bytes = new byte[1024];
+		while ((read = is.read(bytes)) != -1) {
+			out.write(bytes, 0, read);
+		}
+		out.flush();
+		out.close();
+		is.close();
+		os.close();
+		PDImageXObject  pdImage = null;
+		if(StringUtil.isStrContainsWithEqIgnoreCase(image, "png")) {
+			File f = new File(imagePath);
+			BufferedImage bimg1 = ImageIO.read(f);
+			pdImage = LosslessFactory.createFromImage(document, bimg1);
+		}else {
+			pdImage = PDImageXObject.createFromFile(imagePath, document);
+		}
+		PDPage page = document.getPage(pageNumber);
+		PDPageContentStream contentStream = new PDPageContentStream(document, page,AppendMode.APPEND, true);
+		contentStream.drawImage(pdImage, xValue+changedXValue, yValue+changedYValue);
+		contentStream.close();
+		in.close();	
 
+	}
+	public static void pdfTextInserter(PDDocument document,String insertValue, int xValue, int yValue) throws Exception {
+		PDPage page = document.getPage(0);
+	   	PDPageContentStream contentStream = new PDPageContentStream(document, page,AppendMode.APPEND, true);
+		contentStream.beginText();
+		contentStream.setNonStrokingColor(0, 0, 0);
+		File f = new File("E:\\ekyc\\"+"MonospaceTypewriter.ttf");
+		PDFont font1 = PDTrueTypeFont.loadTTF(document, f);
+//		if (resizeRequired > 0) {
+//			contentStream.setCharacterSpacing(0.4f);
+//			if(pageNumber == 2 && StringUtil.isEqual(ColumnName,"reduced_city")) {
+////				contentStream.setFont(font1, 2);
+//				contentStream = changeInputTextSizesForCity(contentStream,font1,insertValue,pageNumber,ColumnName);
+//			}else {
+//				contentStream = changeInputTextSizes(contentStream,font1,insertValue,pageNumber,ColumnName);
+//			}
+//		} else {
+//			contentStream.setFont(font1, 8);
+//			contentStream.setCharacterSpacing(0.4f);
+//		}
+		contentStream.setFont(font1,8);
+		contentStream.setCharacterSpacing(0.4f);
+		contentStream.newLineAtOffset(xValue, yValue);
+		String text = insertValue;
+		contentStream.showText(text);
+		contentStream.endText();
+		contentStream.close();
+	}
 }
